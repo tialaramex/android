781a207ba187569c25ae4050e5fa018c815d81f6
Nick Lamb <git@tree.tlrmx.org>
2010-10-29 10:00:47 +0100

    add generated skeleton for BandCamp one-time-password app

---
+++ BandCamp/src/org/tlrmx/otp/BandCamp.java
@@ -0,0 +1,15 @@
+package org.tlrmx.otp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BandCamp extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
