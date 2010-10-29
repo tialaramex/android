1da0cc4ab40a39e47234ebe2d7a88de590ff9c4e
Nick Lamb <git@tree.tlrmx.org>
2010-10-29 09:57:21 +0100

    customise HelloAndroid app

--- HelloAndroid/src/org/tlrmx/otp/HelloAndroid.java
+++ HelloAndroid/src/org/tlrmx/otp/HelloAndroid.java
@@ -2,14 +2,20 @@ package org.tlrmx.otp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloAndroid extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello Nick, Welcome to Android");
+        setContentView(tv);
     }
 }
+
+/* vi:set ts=8 sts=4 sw=4 et: */
