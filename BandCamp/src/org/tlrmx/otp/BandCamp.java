ab1317fa73246f81c8ec1b45897aa43f77b0651b
Nick Lamb <git@tree.tlrmx.org>
2010-11-18 10:08:59 +0000

    correct mOTP calculation (but example secret is a dummy) and nicer layout

--- BandCamp/src/org/tlrmx/otp/BandCamp.java
+++ BandCamp/src/org/tlrmx/otp/BandCamp.java
@@ -2,14 +2,64 @@ package org.tlrmx.otp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+/** This One Time ... */
 public class BandCamp extends Activity
 {
+    private String secret = "replacewithyours";
+    private String pin = "1111";
+
+    private final static char digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+    private static StringBuilder byteArrayToHex(StringBuilder destination, byte[] source) {
+        destination.ensureCapacity(destination.length() + 2 * source.length);
+        for(int i = 0; i < source.length; ++i) {
+            byte b = source[i];
+            char c2 = digits[b & 0xf];
+            char c1 = digits[((b & 0xf0) >>> 4) & 0xf];
+            destination.append(c1);
+            destination.append(c2);
+        }
+        return destination;
+    }
+
+    private String calculate() {
+        Date now = new Date();
+        long deciseconds = now.getTime() / 10000;
+        String mush = "" + deciseconds + secret + pin;
+
+        MessageDigest md5;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Missing MD5 hash implementation", e);
+        }
+
+        md5.update(mush.getBytes());
+        StringBuilder output = byteArrayToHex(new StringBuilder(), md5.digest());
+        
+        return output.toString().substring(0, 6);
+    }
+
+    TextView codeView;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        codeView = (TextView) findViewById(R.id.code);
+        setCode(calculate());
+    }
+
+    private void setCode(String code) {
+        codeView.setText(code);
     }
 }
+
+/* vi:set ts=8 sts=4 sw=4 et: */
