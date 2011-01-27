bc3c5ac0b6c8b30c7cc31bc6ca1ba573dd6ab7bc
Nick Lamb <git@tree.tlrmx.org>
2011-01-27 12:35:47 +0000

    dialog shows new key when it is reset

--- BandCamp/src/org/tlrmx/otp/BandCamp.java
+++ BandCamp/src/org/tlrmx/otp/BandCamp.java
@@ -1,13 +1,17 @@
 package org.tlrmx.otp;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.os.CountDownTimer;
 import android.content.Context;
+import android.content.DialogInterface;
 
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
@@ -21,12 +25,15 @@ import java.util.Date;
 public class BandCamp extends Activity
 {
     private byte[] secret;
+    private String textSecret = null; /* only set when key is changed */
     private String pin = "1111";
 
     private SecureRandom sr = new SecureRandom();
 
     /* technically it isn't the filename which is secret but its contents */
-    private static String SECRET_FILENAME = "hannigan";
+    private static final String SECRET_FILENAME = "hannigan";
+
+    private static final int NEW_SECRET_DIALOG = 1;
 
     private final static char digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
@@ -132,14 +139,41 @@ public class BandCamp extends Activity
         return true;
     }
 
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog;
+        switch(id) {
+        case NEW_SECRET_DIALOG:
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Secret key: " + textSecret)
+                .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+            dialog = builder.create();
+
+            break;
+        default:
+            dialog = null;
+        }
+
+        return dialog;
+    }
+
+
+    private void alert(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     private void reset() {
         byte[] bytes = new byte[8];
         sr.nextBytes(bytes);
-        String newSecret = byteArrayToHex(new StringBuilder(), bytes).toString();
-        /* should also display this to the user ... */
-        save(newSecret);
-        secret = newSecret.getBytes();
+        textSecret = byteArrayToHex(new StringBuilder(), bytes).toString();
+        save(textSecret);
+        secret = textSecret.getBytes();
         refresh();
+        removeDialog(NEW_SECRET_DIALOG); /* in case there's an old one with the previous key */
+        showDialog(NEW_SECRET_DIALOG);
     }
 
     @Override
